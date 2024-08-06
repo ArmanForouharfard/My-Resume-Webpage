057f76e889a52c46f3c70ad01b99adea5600ff50
Arman forouharfard <129984743+ArmanForouharfard@users.noreply.github.com>
2024-08-06 16:32:39 +0330

    Add files via upload

---
+++ asset/Other projects/Forouhar Industries Website/index 1.jsx
@@ -0,0 +1,68 @@
+// the navbar last child collaps
+function myFunction() {  
+    var x = document.getElementById("mynavbar");  
+    if (x.className === "navbar") {  
+      x.className += " responsive";  
+    } else {  
+      x.className = "navbar";  
+    }  
+  }
+
+  // the Product's page (collapsible3)
+  var coll = document.getElementsByClassName("collapsible1");
+    var i;
+        for (i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.maxHeight){
+                    content.style.maxHeight = null;
+                } else {
+                    content.style.maxHeight = content.scrollHeight + "px";
+                } 
+            });
+        }
+    // the Global project's page (collapsible)
+    var coll = document.getElementsByClassName("collapsible2");
+        var i;
+            for (i = 0; i < coll.length; i++) {
+                coll[i].addEventListener("click", function() {
+                    this.classList.toggle("active");
+                    var content = this.nextElementSibling;
+                    if (content.style.maxHeight){
+                        content.style.maxHeight = null;
+                    } else {
+                        content.style.maxHeight = content.scrollHeight + "px";
+                    } 
+                });
+            }
+
+    // the technical university's page (collapsible)
+    var coll = document.getElementsByClassName("collapsible3");
+        var i;
+            for (i = 0; i < coll.length; i++) {
+                coll[i].addEventListener("click", function() {
+                    this.classList.toggle("active");
+                    var content = this.nextElementSibling;
+                    if (content.style.maxHeight){
+                        content.style.maxHeight = null;
+                    } else {
+                        content.style.maxHeight = content.scrollHeight + "px";
+                    } 
+                });
+            }
+
+    // About us & our History's page (collapsible4)
+    var coll = document.getElementsByClassName("collapsible4");
+        var i;
+            for (i = 0; i < coll.length; i++) {
+                coll[i].addEventListener("click", function() {
+                    this.classList.toggle("active");
+                    var content = this.nextElementSibling;
+                    if (content.style.maxHeight){
+                        content.style.maxHeight = null;
+                    } else {
+                        content.style.maxHeight = content.scrollHeight + "px";
+                    } 
+                });
+            }
