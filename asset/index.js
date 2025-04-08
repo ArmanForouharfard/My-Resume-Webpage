4ccce0ed5ed98eb8eaa5fb702bb2c81e8220b159
Arman forouharfard <129984743+ArmanForouharfard@users.noreply.github.com>
2025-04-08 18:55:42 +0330

    Update index.js

--- asset/index.js
+++ asset/index.js
@@ -1,13 +1,15 @@
         /*navbar menu*/
         function openNav() {
             document.getElementById("mySidenav").style.width = "250px";
-            document.getElementById("main").style.marginLeft = "250px";
+            //document.getElementById("main").style.marginLeft = "250px";
           }
 
           function closeNav() {
             document.getElementById("mySidenav").style.width = "0";
             document.getElementById("main").style.marginLeft= "0";
           }
+
+          
 
       
       /*slideshow*/
